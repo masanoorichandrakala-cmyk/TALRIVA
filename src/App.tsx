2da36db8c58a0a8f36082b6fc4d955449e69efcf
masanoorichandrakala-cmyk <masanoori.chandrakala@yahoo.com>
2026-07-11 12:11:15 +0530

    Change ELEVENLABS_KEY to a new API key
    Updated ELEVENLABS_KEY to a new value.

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useState, useEffect, useRef } from "react";
 
 const CAL = "https://cal.com/chandrakala.m/discovery-call";
 const LINKEDIN = "https://www.linkedin.com/company/workriva";
-const ELEVENLABS_KEY = "4084d953fb5b92c7bbeeca332b2b43d013bd7d769ccc1330e55b5e61bcb57edc";
+const ELEVENLABS_KEY = "d968bd8b5342da6a6ef92204b1d9c51f5afa4d8dbe8120b35e01b045786847ae";
 const VOICE_ID = "21m00Tcm4TlvDq8ikWAM";
 
 const T = {
